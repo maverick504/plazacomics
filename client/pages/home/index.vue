e84bd0fd653b596e6bca4d3ffbf3f73a694a3d07
Emiliano Jaimes <emilianomangaka@gmail.com>
2019-08-06 03:08:17 -0300

    Updated call to action in home.

--- client/pages/home/index.vue
+++ client/pages/home/index.vue
@@ -14,10 +14,10 @@
         </div>
       </div>
     </div>
-    <div class="bg-primary text-light pt-xl pb-xl">
+    <div class="bg-primary text-light py-xl">
       <div class="container">
         <div class="col-6 col-md-12">
-          <h3>Publica tu cómic en PlazaComics!</h3>
+          <h3>¡Publica tu cómic en PlazaComics!</h3>
           <p>Publica tu cómic gratis en una plataforma optimizada para la lectura de cómics.</p>
           <router-link :to="{ name: 'info.publishing' }" class="btn btn-lg">
             <information-outline-icon class="mr-sm" /> Saber más
